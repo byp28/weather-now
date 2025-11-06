55ebf8e2c8fc247e9de6bc1168a81ed327b3008c
byp28 <yannight28@gmail.com>
2025-11-06 11:33:12 +0100

    066

--- src/components/HorlyForecast/DaysMenu.vue
+++ src/components/HorlyForecast/DaysMenu.vue
@@ -4,13 +4,14 @@
 </script>
 
 <template>
-    <section class="w-40 text-white bg-[#25253F] rounded-sm p-2 flex flex-col gap-2 items-center">
-        <span class="w-full p-2 text-sm font-semibold cursor-pointer rounded-sm  hover:bg-[#2F2F49] hover:border-white ">Switch to Imperial</span>
-        <span class="w-full p-2 text-sm font-semibold cursor-pointer rounded-sm  hover:bg-[#2F2F49] hover:border-white ">Switch to Imperial</span>
-        <span class="w-full p-2 text-sm font-semibold cursor-pointer rounded-sm  hover:bg-[#2F2F49] hover:border-white ">Switch to Imperial</span>
-        <span class="w-full p-2 text-sm font-semibold cursor-pointer rounded-sm  hover:bg-[#2F2F49] hover:border-white ">Switch to Imperial</span>
-        <span class="w-full p-2 text-sm font-semibold cursor-pointer rounded-sm  hover:bg-[#2F2F49] hover:border-white ">Switch to Imperial</span>
-
+    <section class="w-40 text-white border-1 border-[#2F2F49] bg-[#25253F] rounded-sm p-1 flex flex-col gap-2 items-center">
+        <span class="w-full p-2 text-sm font-semibold cursor-pointer rounded-sm  hover:bg-[#2F2F49] hover:border-white ">Monday</span>
+        <span class="w-full p-2 text-sm font-semibold cursor-pointer rounded-sm  hover:bg-[#2F2F49] hover:border-white ">Tuesday</span>
+        <span class="w-full p-2 text-sm font-semibold cursor-pointer rounded-sm  hover:bg-[#2F2F49] hover:border-white ">Wednesday</span>
+        <span class="w-full p-2 text-sm font-semibold cursor-pointer rounded-sm  hover:bg-[#2F2F49] hover:border-white ">Thursday</span>
+        <span class="w-full p-2 text-sm font-semibold cursor-pointer rounded-sm  hover:bg-[#2F2F49] hover:border-white ">Friday</span>
+        <span class="w-full p-2 text-sm font-semibold cursor-pointer rounded-sm  hover:bg-[#2F2F49] hover:border-white ">Saturday</span>
+        <span class="w-full p-2 text-sm font-semibold cursor-pointer rounded-sm  hover:bg-[#2F2F49] hover:border-white ">Sunday</span>
     </section>
 </template>
 
--- src/components/HourlyForecast.vue
+++ src/components/HourlyForecast.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import DaysMenu from './HorlyForecast/DaysMenu.vue';
 import HourForecast from './HourForecast.vue';
 
+const hiddenSubmenu = ref(false)
+//const loading = ref(true)
 
+const toggleMenu = ()=>{
+    hiddenSubmenu.value = !hiddenSubmenu.value
+}
 
 </script>
 
@@ -9,9 +16,12 @@ import HourForecast from './HourForecast.vue';
     <div class="w-160 bg-[#25253F] h-full p-5 rounded-lg flex gap-4 flex-col">
         <div class="w-full flex justify-between items-center">
             <span class="text-white font-semibold">Hourly Forecast</span>
-            <span class="flex gap-3 items-center px-3 py-2 bg-[#3C3A5E] rounded-sm">
-                <span class="text-white font-semibold">-</span>
+            <span @click.self="toggleMenu" class="flex gap-3 items-center px-3 py-2 bg-[#3C3A5E] rounded-sm relative">
+                <span class="text-white font-semibold">Monday</span>
                 <img src="/assets/images/icon-dropdown.svg" alt="dropdown">
+                <div  v-if="hiddenSubmenu" class="absolute right-0 -bottom-80 ">
+                    <DaysMenu/>
+                </div>
             </span>
         </div>
         <section class="w-full flex flex-col gap-4">
